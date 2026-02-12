a9e6879df3658b8fce61ce55281e737aca771f98
kwonjiwon <kjw81024@gmail.com>
2026-02-12 09:53:43 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        int sum = 0;
+        int len = arr.length;
+        
+        for (int i=0; i<len; i++){
+            sum += arr[i];
+        }
+        
+        //if (sum == 0) return 0;
+        
+        answer = (double) sum / len;
+        return answer;
+    }
+}
